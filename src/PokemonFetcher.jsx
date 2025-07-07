3f24bf03cc907f8e59b1704e25330ae6f0f42dea
nicolas <nikolascisternas.daza@gmail.com>
2025-07-07 17:19:41 -0400

    comit 2

--- src/PokemonFetcher.jsx
+++ src/PokemonFetcher.jsx
@@ -33,25 +33,22 @@ const PokemonFetcher = () => {
   const [pokemonesAleatorios, setPokemonesAleatorios] = useState([]);
 
   const traducirTipos = (tipos) => {
-    return tipos.map(tipo => tipoMap[tipo] || tipo);  
+    return tipos.map(tipo => tipoMap[tipo] || tipo);
   };
 
   const debounceSearch = useCallback(
     (searchTerm) => {
       if (!searchTerm) {
-        setPokemones([]); // Si el campo de búsqueda está vacío no muestra resultados
+        setPokemones([]);
         return;
       }
 
       setCargando(true);
 
-     
       const fetchPokemones = async () => {
         try {
           const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`);
-          if (!response.ok) {
-            throw new Error('Error al cargar los Pokémon');
-          }
+          if (!response.ok) throw new Error('Error al cargar los Pokémon');
           const data = await response.json();
 
           const fetchedPokemones = [];
@@ -63,7 +60,7 @@ const PokemonFetcher = () => {
               id: pokemonData.id,
               nombre: pokemonData.name,
               imagen: pokemonData.sprites.front_default,
-              tipos: traducirTipos(pokemonData.types.map(typeInfo => typeInfo.type.name)), 
+              tipos: traducirTipos(pokemonData.types.map(typeInfo => typeInfo.type.name)),
             });
           }
           setPokemones(fetchedPokemones);
@@ -82,7 +79,7 @@ const PokemonFetcher = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       debounceSearch(filtro);
-    }, 500); 
+    }, 500);
 
     return () => clearTimeout(timer);
   }, [filtro, debounceSearch]);
@@ -95,22 +92,20 @@ const PokemonFetcher = () => {
 
         const pokemonIds = new Set();
         while (pokemonIds.size < 4) {
-          const randomId = Math.floor(Math.random() * 898) + 1; // número  de Pokémon en la PokeAPI
+          const randomId = Math.floor(Math.random() * 898) + 1;
           pokemonIds.add(randomId);
         }
 
         const aleatorios = [];
         for (const id of pokemonIds) {
           const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-          if (!response.ok) {
-            throw new Error(`Error al cargar el Pokémon con ID ${id}: ${response.statusText}`);
-          }
+          if (!response.ok) throw new Error(`Error al cargar el Pokémon con ID ${id}`);
           const data = await response.json();
           aleatorios.push({
             id: data.id,
             nombre: data.name,
             imagen: data.sprites.front_default,
-            tipos: traducirTipos(data.types.map(typeInfo => typeInfo.type.name)), 
+            tipos: traducirTipos(data.types.map(typeInfo => typeInfo.type.name)),
           });
         }
         setPokemonesAleatorios(aleatorios);
@@ -139,36 +134,37 @@ const PokemonFetcher = () => {
 
   return (
     <div className='pokemon-container'>
-      <h2>Tus 4 Pokémon Aleatorios</h2>
-      <img className="pokeball" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/029b8bd9-cb5a-41e4-9c7e-ee516face9bb/dayo3ow-7ac86c31-8b2b-4810-89f2-e6134caf1f2d.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAyOWI4YmQ5LWNiNWEtNDFlNC05YzdlLWVlNTE2ZmFjZTliYlwvZGF5bzNvdy03YWM4NmMzMS04YjJiLTQ4MTAtODlmMi1lNjEzNGNhZjFmMmQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ooubhxjHp9PIMhVxvCFHziI6pxDAS8glXPWenUeomWs" alt="Descripción de la imagen"></img>
-      <div className="pokemon-list random-list">
-        {pokemonesAleatorios.map(pokemon => (
-          <div key={pokemon.id} className="pokemon-card">
-            <h3>{pokemon.nombre.charAt(0).toUpperCase() + pokemon.nombre.slice(1)}</h3>
-            <img src={pokemon.imagen} alt={pokemon.nombre} /> 
-            <p>
-              <strong>Tipos:</strong> {pokemon.tipos.join(', ')}  
-            </p>
-          </div>
-        ))}
-      </div>
-
       <h3>- Buscar pokémon:</h3>
-      <div>
+      <div className="search-bar">
         <input
           type="text"
           placeholder="Buscar Pokémon"
           value={filtro}
           onChange={(e) => setFiltro(e.target.value)}
+          style={{ borderRadius: '0px' }}
         />
-        <select onChange={(e) => setCriterioFiltro(e.target.value)}>
+        <select
+          onChange={(e) => setCriterioFiltro(e.target.value)}
+          style={{ borderRadius: '0px' }}
+        >
           <option value="nombre">Por Nombre</option>
           <option value="tipo">Por Tipo</option>
         </select>
       </div>
-      <img className='otrogif' src="/src/multimedia/gif.gif" alt="" />
 
-      
+      <h2>Tus 4 Pokémon Aleatorios </h2>
+      <div className="pokemon-list random-list">
+        {pokemonesAleatorios.map(pokemon => (
+          <div key={pokemon.id} className="pokemon-card">
+            <h3>{pokemon.nombre.charAt(0).toUpperCase() + pokemon.nombre.slice(1)}</h3>
+            <img src={pokemon.imagen} alt={pokemon.nombre} />
+            <p><strong>Tipos:</strong> {pokemon.tipos.join(', ')}</p>
+          </div>
+        ))}
+      </div>
+
+      <img className='otrogif' src="/src/multimedia/gif.gif" alt="gif" />
+
       {filtro && (
         <>
           <h2>Tus Pokémon Filtrados</h2>
@@ -178,9 +174,7 @@ const PokemonFetcher = () => {
                 <div key={pokemon.id} className="pokemon-card">
                   <h3>{pokemon.nombre.charAt(0).toUpperCase() + pokemon.nombre.slice(1)}</h3>
                   <img src={pokemon.imagen} alt={pokemon.nombre} />
-                  <p>
-                    <strong>Tipos:</strong> {pokemon.tipos.join(', ')}  
-                  </p>
+                  <p><strong>Tipos:</strong> {pokemon.tipos.join(', ')}</p>
                 </div>
               ))
             ) : (
